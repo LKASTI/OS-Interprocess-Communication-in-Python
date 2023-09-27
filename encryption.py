072b8ac27ce54eab7cd1729a6d91251f07eaf5ca
Landin Kasti <lkastiman@gmail.com>
2023-09-27 09:51:14 -0500

    Added upper case functionality to encryption/decryption. Special case: passkey is shorter than word

--- encryption.py
+++ encryption.py
@@ -4,29 +4,39 @@ import sys
 line = ""
 passkey = ""
 
+
+# TODO: what if the the word to enc/dec is longer than the passkey?
 def encrypt(word):
     encrypted = []
     for i in range(0, len(word)):
-        ascii_val = ord(word[i]) + (ord(passkey[i].lower()) - ord('a'))
+        ascii_val = ord(word[i].lower()) + (ord(passkey[i].lower()) - ord('a'))
 
         if ascii_val > 122:
             diff = ascii_val - 122
             ascii_val = diff + 96
-
-        encrypted.append(chr(ascii_val))
+        
+        # Is the original letter in Upper case?
+        if ord(word[i]) <= 90:
+            encrypted.append(chr(ascii_val - 32))
+        else:
+            encrypted.append(chr(ascii_val))
 
     return "".join(encrypted)
 
 def decrypt(word):
     decrypted = []
     for i in range(0, len(word)):
-        ascii_val = ord(word[i]) - (ord(passkey[i].lower()) - ord('a'))
+        ascii_val = ord(word[i].lower()) - (ord(passkey[i].lower()) - ord('a'))
 
         if ascii_val < 97:
             diff = 97 - ascii_val
             ascii_val = 123 - diff
         
-        decrypted.append(chr(ascii_val))
+        # Is the original letter in Upper case?
+        if ord(word[i]) <= 90:
+            decrypted.append(chr(ascii_val - 32))
+        else:
+            decrypted.append(chr(ascii_val))
 
     return "".join(decrypted)
 
@@ -47,8 +57,15 @@ while True:
     elif not passkey:
         sys.stdout.write("ERROR: A passkey must be set before encrypting/decrypting")
     elif cmd == "ENCRYPT":
-        pass
+        # encrypt the string
+        enc_string = encrypt(arg)
+
+        sys.stdout.write(f"RESULT {enc_string}")
     elif cmd == "DECRYPT":
-        pass
+        # decrypt the string
+        dec_string = decrypt(arg)
+
+        sys.stdout.write(f"RESULT {dec_string}")
+
     
 
