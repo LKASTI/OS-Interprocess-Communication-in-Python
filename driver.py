fe3884ef357901e5fec1d34f7587ccf1c97e1d88
Landin Kasti <lkastiman@gmail.com>
2023-10-05 18:17:19 -0500

    Implemented using history, and fixed minor issues with formatting. Also rewrote driver to take cmd and args separately.

--- driver.py
+++ driver.py
@@ -22,55 +22,80 @@ def display_history():
     print("------------------------------------------------History------------------------------------------------\n\n")
     # Print all options in history
     for i,word in enumerate(history):
+        if i == len(history)-1:
+            continue
         print(f"{i+1}. {word}\n")
 
-    print("-------------------------------------------------------------------------------------------------------\n")
+    print("-------------------------------------------------------------------------------------------------------")
     
 def use_history():
-    print("Would you like to use the history? (Y/N) ")
+    sys.stdout.write("Would you like to use the history? (Y/N) ")
+    sys.stdout.flush()
     ans = sys.stdin.readline().rstrip()
 
     if ans.lower() == "y":
         display_history()
+        print(f"{len(history)}. (Go Back)")
+        print("-------------------------------------------------------------------------------------------------------")
 
         num = -1
         while 0 > num or num >= len(history):
-            print("Choose a word: ")
-            num = sys.stdin.readline().rstrip()
-            if int(num) < 0 or int(num) >= len(history):
-                print("Please enter the number representing the word from the listed strings.\n")            
-        return history[int(num) - 1] 
+            sys.stdout.write("Choose a word: ")
+            sys.stdout.flush()
+            num = int(sys.stdin.readline().rstrip())
+            if num == len(history):
+                # -2 means to go back and prompt for user to enter new string
+                return -2  
+            if num <= 0 or num > len(history):
+                print("Please enter the number representing the word from the listed strings.\n")     
+        return history[num - 1] 
+    # -1 means they won't use history so continue asking for input
+    return -1
 
 while True:
     sys.stdout.write(menu_text)
     sys.stdout.write("\nEnter a command: ")
 
     # Waiting for user to write to terminal
-    line = sys.stdin.readline().rstrip()
-    sys.stdout.write("\n")
+    cmd = sys.stdin.readline().rstrip()
 
-    if line.find(' ') == -1:
-        if line == "quit":
-            logger.stdin.write("STOPPED Logging Stopped\n")
-            encrypter.stdin.write("QUIT\n")
-            logger.stdin.flush()
-            encrypter.stdin.flush()
-            logger.wait()
-            encrypter.wait()
-            sys.exit(0)
-        elif line == "history":
-            # TODO: display history
-            display_history()
-            logger.stdin.write("HISTORY SUCCESS\n")
-        else:
-            logger.stdin.write(f"UNKNOWN_CMD {line}\n")
-            sys.stdout.write("Please enter one of the listed commands.\n")    
+
+    if cmd == "quit":
+        logger.stdin.write("STOPPED Logging Stopped\n")
+        encrypter.stdin.write("QUIT\n")
+        logger.stdin.flush()
+        encrypter.stdin.flush()
+        logger.wait()
+        encrypter.wait()
+        sys.exit(0)
+    elif cmd == "history":
+        # Display history
+        display_history()
+        logger.stdin.write("HISTORY SUCCESS\n")  
     else:
-        cmd = line[:line.index(' ')]
-        arg = line[line.index(' ') + 1:]
+        arg = ""
+        # Provide user option of using a string in history or entering a new string
+        if cmd == "password" or cmd == "encrypt" or cmd == "decrypt":
+            if len(history) > 1:
+                temp = use_history()
+                if type(temp) == str:
+                    arg = temp
+                # Log user went back in history
+                elif temp == -2:
+                    logger.stdin.write(f"ATTEMPTED_USE_HISTORY {cmd}\n")
+                    logger.stdin.flush()
+                    logger.stdin.write(f"GO_BACK SUCCESS\n")
+                    logger.stdin.flush()
+                # When temp == -1 then continue as normal
 
         if cmd == "password":
-            # TODO: provide user option of using a string in history or entering a new string
+            # Ask user to enter an argument if arg is empty            
+            if arg == "":
+                sys.stdout.write("Enter a password: ")
+                sys.stdout.flush()
+                arg = sys.stdin.readline().rstrip()
+                sys.stdout.write("\n")
+                sys.stdout.flush()
 
             # Log password action 
             logger.stdin.write(f"SET_PASSWORD {arg}\n")
@@ -78,9 +103,6 @@ while True:
 
             # Check if argument is a latin letter a-z, A-Z
             if bool(re.match(r'^[a-zA-z]+$', arg)):
-                # Store password in history
-                if arg not in history:
-                    history.appendleft(arg)
                 # Send password to encrypter and read response
                 encrypter.stdin.write(f"PASSKEY {arg}\n")
                 encrypter.stdin.flush()
@@ -92,11 +114,17 @@ while True:
             else:
                 # Log failed response and print to stdout
                 logger.stdin.write("SET_PASSWORD ERROR: password must contain letters only\n")
-                print("ERROR: password must contain letters only")
+                sys.stdout.write("ERROR: password must contain letters only")
+                sys.stdout.flush()
         elif cmd == "encrypt" or cmd =="decrypt":
-            # TODO: Provide user option of using a string in history or entering a new string
+            # Ask user to enter an argument if arg is empty            
+            if arg == "":
+                sys.stdout.write("Enter a word to encrypt/decrypt: ")
+                sys.stdout.flush()
+                arg = sys.stdin.readline().rstrip()
+                sys.stdout.write("\n")
 
-            # TODO: Save the string that will be encrypted into history 
+            # Save the string that will be encrypted into history 
             if arg not in history:
                 history.appendleft(arg)
 
@@ -104,7 +132,6 @@ while True:
                 # Log the action
                 logger.stdin.write(f"ENCRYPT {arg}\n")
                 logger.stdin.flush()
-
 
                 # Send string to encrypter and read response
                 encrypter.stdin.write(f"ENCRYPT {arg}\n")
@@ -115,7 +142,11 @@ while True:
                 if "ERROR" in enc_response:
                     logger.stdin.write(f"ENCRYPT {enc_response}\n")
                 else:
-                    logger.stdin.write(f"ENCRYPT SUCCESS: {enc_response[enc_response.index(' ') + 1:]}\n")
+                    result = enc_response[enc_response.index(' ') + 1:]
+                    logger.stdin.write(f"ENCRYPT SUCCESS: {result}\n")
+                    # Store result in history
+                    if result not in history:
+                        history.appendleft(result)
                 print(enc_response)
             else:
                 # Log the action
@@ -131,17 +162,22 @@ while True:
                 if "ERROR" in enc_response:
                     logger.stdin.write(f"DECRYPT {enc_response}\n")
                 else:
-                    logger.stdin.write(f"DECRYPT SUCCESS: {enc_response[enc_response.index(' ') + 1:]}\n")
+                    result = enc_response[enc_response.index(' ') + 1:]
+                    logger.stdin.write(f"DECRYPT SUCCESS: {result}\n")
+                    # Store result in history
+                    if result not in history:
+                        history.appendleft(result)
                 print(enc_response)
         else:
             # Log unknown command
-            logger.stdin.write(f"UNKNOWN_CMD_{cmd.upper()} UNKNOWN_ARG_{arg}\n")
+            logger.stdin.write(f"UNKNOWN_CMD {cmd}\n")
             sys.stdout.write("Please enter one of the listed commands.\n")    
 
     logger.stdin.flush()
 
     # Press enter to continue
-    print("Press enter to continue...")
+    sys.stdout.write("Press enter to continue...")
+    sys.stdout.flush()
     sys.stdin.readline()
     
     sys.stdout.write("\n")
